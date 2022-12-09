967e52905f160dd2eb9ed55fd867e4a2779c7935
yebinbin <itdeveloper@hello-tech.com>
2022-12-09 16:53:36 +0800

    init

---
+++ src/js/index/theme.js
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2022-11-26 17:10:31
+ * @LastEditors: Leo
+ * @LastEditTime: 2022-12-08 13:01:03
+ * @FilePath: \shopify3.0\src\js\index\theme.js
+ */
+import $ from 'jquery'
+import '../../css/theme.css'
+window.jQuery = window.$ = $
+__webpack_public_path__ = window.__webpack_public_path__;
