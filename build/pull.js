1e20c21b16ba473079077232811017e601d9e2e0
yebinbin <itdeveloper@hello-tech.com>
2022-12-21 17:36:23 +0800

    add base css

--- build/pull.js
+++ build/pull.js
@@ -5,17 +5,19 @@
  * @FilePath: \3.0-manger\build\pull.js
  * @description: 拉取指定店铺模板
  */
-const shell = require("shelljs");
-const exec = shell.exec;
+const shell = require('shelljs');
+
+const { exec } = shell;
 const read = require('read-yaml');
+
 const config = read.sync('config.yml');
-let name = process.argv[2] || 'dev'; // 第一个参数传店铺
-let branch = process.argv[3] || 'master'; // 第二个参数传分支名称
+const name = process.argv[2] || 'dev'; // 第一个参数传店铺
+const branch = process.argv[3] || 'master'; // 第二个参数传分支名称
 
-if(!config[name]) {
-    shell.echo(`Error: 店铺参数${name}错误`);
-    shell.exit(1);
+if (!config[name]) {
+  shell.echo(`Error: 店铺参数${name}错误`);
+  shell.exit(1);
 }
-console.log(`${name}店铺${branch}拉取中...`)
+console.log(`${name}店铺${branch}拉取中...`);
 
-exec(`git subtree pull --prefix=theme/${name}/${branch} ${name.toUpperCase()} ${branch} --squash`)
+exec(`git subtree pull --prefix=theme/${name}/${branch} ${name.toUpperCase()} ${branch} --squash`);
