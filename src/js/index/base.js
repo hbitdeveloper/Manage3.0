a6c2fd5ba7d386a28654a14e1cfaa09af11e3c04
zzl-leo <1196567505@qq.com>
2022-12-28 09:49:57 +0800

    首页header-dropmenu

--- src/js/index/base.js
+++ src/js/index/base.js
@@ -1,14 +1,17 @@
 /*
  * @Date: 2022-12-27 15:11:35
  * @LastEditors: Leo
- * @LastEditTime: 2022-12-27 15:11:35
+ * @LastEditTime: 2022-12-28 09:46:01
  * @FilePath: \3.0-manger\src\js\index\base.js
+ * @description: globaljs
  */
 import DetailsDisclosure from '../components/details-disclosure'
+import DetailsModal from '../components/details-modal'
+import PredictiveSearch from '../components/private-search'
 
 customElements.define('details-disclosure', DetailsDisclosure);
-
-
+customElements.define('details-modal', DetailsModal);
+customElements.define('predictive-search', PredictiveSearch);
 
 // Modals
 (function (e, t) {
