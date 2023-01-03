4d12f45148aa214f6e2707ac5a544bc10b854e70
yebinbin <itdeveloper@hello-tech.com>
2023-01-03 18:01:29 +0800

    user-guide.js

--- src/js/index/user-guide.js
+++ src/js/index/user-guide.js
@@ -1,24 +1,52 @@
 class UserGuide {
   constructor(el) {
     this.DOM = { el };
-    this.config = {};
+    this.config = { pdf: null };
     this.init();
   }
 
   init() {
+    function tabContent(ul, data) {
+      let li = "";
+      const download = document.querySelector(".tab-content li a").innerText;
+      data.forEach((item) => {
+        li += `<li>
+          <i class="img-icon iconfont jackery-icon-pdf"></i>
+          <b class="h5">${item.file_name}</b>
+          <a class="h5" href="${item.link}"><i class="iconfont jackery-icon-download"></i>${download}</a>
+        </li>`;
+      })
+      ul.innerHTML = li;
+    }
+    function search() {
+      const input = this.DOM.el.querySelector(".search-box input");
+      const { type } = this.DOM.el.querySelector(".tab li.on").dataset;
+      if (input.value) {
+        tabContent(this.DOM.el.querySelector(`.tab-content .${type}`), this.config.pdf[type].filter((v) => new RegExp(input.value).test(v.file_name)))
+      } else {
+        tabContent(this.DOM.el.querySelector(`.tab-content .${type}`), this.config.pdf[type])
+      }
+    }
     function tab(li, content) {
       li.forEach((item, index, arr) => {
         item.onclick = () => {
           arr.forEach((v) => { v.classList.remove("on") });
           content.forEach((v) => { v.classList.remove("on") });
           item.classList.add("on");
-          content[index].classList.add("on")
+          content[index].classList.add("on");
+          search()
         }
       })
     }
-    tab(document.querySelectorAll('.pdf .tab li'), document.querySelectorAll('.pdf .tab-content'));
+
+    document.addEventListener('DOMContentLoaded', () => {
+      this.config.pdf = JSON.parse(document.querySelector("#user-guide-json").innerText);
+      tab(document.querySelectorAll('.pdf .tab li'), document.querySelectorAll('.pdf .tab-content'));
+
+      this.DOM.el.querySelector(".search-box button").onclick = () => search()
+    })
   }
 }
 
-const userGuide = new UserGuide();
+const userGuide = new UserGuide(document.querySelector(".manuals.pdf"));
 export default userGuide;
